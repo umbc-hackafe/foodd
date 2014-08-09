f07eb7cb3613655b00545ab43bcec548d1726e03
Alexander Bauer <sasha@crofter.org>
2014-08-09 00:17:50 -0400

    Finish out database model

--- foodd/models.py
+++ foodd/models.py
@@ -9,12 +9,32 @@ DIMENSIONS = (
         (2, "Mass")
 )
 
-class Dimensionality(models.Model):
-    dimension = models.IntegerField(choices=DIMENSIONS)
-
 class AbstractIngredient(models.Model):
     name           = models.CharField(max_length=255)
     provides       = models.ManyToManyField("self")
     properties     = models.ManyToManyField(Property)
-    dimensionality = models.ForeignKey(Dimensionality)
-    details        = models.TextField()
+    dimensionality = models.IntegerField(choices=DIMENSIONS)
+    details        = models.TextField(null=True)
+
+class Package(models.Model):
+    ingredient = models.ForeignKey(AbstractIngredient)
+    name       = models.CharField(max_length=255)
+    uid        = models.IntegerField()
+    netamount  = models.FloatField()
+    tareamount = models.FloatField()
+    details    = models.TextField(null=True)
+
+class Stock(models.Model):
+    package   = models.ForeignKey(Package)
+    remaining = models.FloatField()
+    expiry    = models.DateField(null=True)
+
+class RecipeIngredient(models.Model):
+    ingredient = models.ForeignKey(AbstractIngredient)
+    properties = models.ManyToManyField(Property, null=True)
+    optional   = models.BooleanField(default=False)
+    amount     = models.FloatField()
+
+class Recipe(models.Model):
+    ingredients     = models.ManyToManyField(RecipeIngredient)
+    instructions    = models.TextField(null=True)
