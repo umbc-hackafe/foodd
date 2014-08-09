e0bf2638f944bddd7ddb053c295b4acb578351b6
Dylan Whichard <dylan@whichard.com>
2014-08-09 00:50:38 -0400

    Add stringification to property model

--- foodd/models.py
+++ foodd/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Property(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self): return self.name
+
 DIMENSIONS = (
         (0, "Discrete"),
         (1, "Volume"),
