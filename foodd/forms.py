15e3d37ee2951cd261fbe69038e68299dcf33178
Dylan Whichard <dylan@whichard.com>
2014-08-09 18:17:11 -0400

    Add forms for everything

--- foodd/forms.py
+++ foodd/forms.py
@@ -1,8 +1,33 @@
 from django.forms import ModelForm
 from foodd.models import *
 
+class PropertyForm(ModelForm):
+    class Meta:
+        model = Property
+        fields = [ 'name' ]
+
+class IngredientForm(ModelForm):
+    class Meta:
+        model = AbstractIngredient
+        fields = [ 'name', 'provides', 'properties', 'dimensionality', 'details' ]
+
 class ProductForm(ModelForm):
     class Meta:
         model = Product
         fields = [ 'ingredient', 'name', 'uid', 'netamount',
                 'tareamount', 'details' ]
+
+class StockForm(ModelForm):
+    class Meta:
+        model = Stock
+        fields = [ 'product', 'remaining', 'expiry' ]
+
+class RecipeIngredientForm(ModelForm):
+    class Meta:
+        model = RecipeIngredient
+        fields = [ 'ingredient', 'properties', 'optional', 'amount' ]
+
+class RecipeForm(ModelForm):
+    class Meta:
+        model = Recipe
+        fields = [ 'name', 'ingredients', 'instructions' ]
