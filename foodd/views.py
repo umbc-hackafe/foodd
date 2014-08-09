c9c104ff20a8a1f26dc1a9c693723b19d0d55f76
Alexander Bauer <sasha@crofter.org>
2014-08-09 02:11:45 -0400

    Serialize recipes to JSON properly

--- foodd/views.py
+++ foodd/views.py
@@ -1,7 +1,7 @@
-from django.utils import simplejson
+from django.core import serializers
 from django.http import HttpResponse
 from foodd.models import Recipe
 
 def list_recipes(request):
-    recipes = Recipe.objects.all()
-    return HttpResponse(simplejson.dumps(list(recipes)), mimetype="application/json")
+    return HttpResponse(serializers.serialize("json",
+        Recipe.objects.all()), mimetype="application/json")
